9f96080a7133990a31351b840f89bba1f3a3814e
inakam <ibuki-nakamura@m3.com>
2025-06-25 23:35:59 +0900

    feat: 意図的な遅延を削除

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -1,13 +1,6 @@
 /// <reference types="@types/serviceworker" />
-import PQueue from 'p-queue';
 
-import { jitter } from './jitter';
 import { transformJpegXLToBmp } from './transformJpegXLToBmp';
-
-// ServiceWorker が負荷で落ちないように並列リクエスト数を制限する
-const queue = new PQueue({
-  concurrency: 5,
-});
 
 self.addEventListener('install', (ev: ExtendableEvent) => {
   ev.waitUntil(self.skipWaiting());
@@ -18,17 +11,10 @@ self.addEventListener('activate', (ev: ExtendableEvent) => {
 });
 
 self.addEventListener('fetch', (ev: FetchEvent) => {
-  ev.respondWith(
-    queue.add(() => onFetch(ev.request), {
-      throwOnTimeout: true,
-    }),
-  );
+  ev.respondWith(onFetch(ev.request));
 });
 
 async function onFetch(request: Request): Promise<Response> {
-  // サーバーの負荷を分散するために Jitter 処理をいれる
-  await jitter();
-
   const res = await fetch(request);
 
   if (res.headers.get('Content-Type') === 'image/jxl') {
